ff7cd1c9b810663b7816b04c17d911437c8d9ed3
MDo <diallo.mamadou099@gmail.com>
2016-01-17 16:36:20 +0100

    premier commit

---
+++ src/main/java/fr/esiag/tp1/App.java
@@ -0,0 +1,14 @@
+package fr.esiag.tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //Bonjour comment ça
+    }
+}
